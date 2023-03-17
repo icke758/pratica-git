0ccba6c8841c810654e64b32ed017e15dc2f0951
Zé <ickenaol@gmail.com>
2023-03-17 15:52:45 -0300

    Atualização de linhas

--- geragrafico.py
+++ geragrafico.py
@@ -9,7 +9,9 @@ def main():
     valores = leitor.getValores()
     print(valores)
     
-    plt.plot(valores)
+    for serie in valores:
+       plt.plot(serie)
+
 
     plt.ylabel('Valores de entrada')
     plt.xlabel('Amostragem')
