f65270bd306004e7b891f4414eae85d77ee93ea6
José Henrique  Noal <2020307260@FARROUPILHA.IFRS.EDU.BR>
2023-03-17 15:10:51 -0300

    Plota Gráfico

--- geragrafico.py
+++ geragrafico.py
@@ -1,19 +1,15 @@
-class LeitorArquivo:
-    def __init__(self, nomeArquivo):
-        self.arq = open(nomeArquivo, 'r')
-        self.valores = [float(x) for x in self.arq.readline().split()]
-   
-    def getValores(self):
-        return self.valores
-        
-        
 from leitorarquivo import LeitorArquivo
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 
 def main():
     leitor = LeitorArquivo('data.txt')
     valores = leitor.getValores()
     print(valores)
-
+    
+    plt.plot(valores)
+    plt.show()
 
 main()
